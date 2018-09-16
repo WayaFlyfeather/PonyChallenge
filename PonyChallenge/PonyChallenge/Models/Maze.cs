21f332abda2cc2682cc0aec006bd7999dc40db7c
Jon Robinson Levy <jon.r.levy@gmail.com>
2018-09-16 12:12:05 +0200

    Starting on service and models

---
+++ PonyChallenge/PonyChallenge/Models/Maze.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PonyChallenge.Models
+{
+    public class Maze
+    {
+        [JsonProperty("maze-id")]
+        public string MazeId { get; set; }
+
+        [JsonProperty("maze-width")]
+        public int MazeWidth { get; set; }
+
+        [JsonProperty("maze-height")]
+        public int MazeHeight { get; set; }
+
+        [JsonProperty("maze-player-name")]
+        public string MazePlayerName { get; set; }
+
+        [JsonProperty("difficulty")]
+        public int Difficulty { get; set; }
+    }
+}
---
+++ PonyChallenge/PonyChallenge/Services/IPonyMazeService.cs
@@ -0,0 +1,13 @@
+﻿using PonyChallenge.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PonyChallenge.Services
+{
+    interface IPonyMazeService
+    {
+        Task<Maze> CreateMaze(Maze maze);
+    }
+}
