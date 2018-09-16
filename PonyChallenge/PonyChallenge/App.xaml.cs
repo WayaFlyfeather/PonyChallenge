92ad4d01b0a3d4dd126a6680cc43f4b6634f2617
Jon Robinson Levy <jon.r.levy@gmail.com>
2018-09-16 11:41:06 +0200

    Basic structure

--- PonyChallenge/PonyChallenge/App.xaml.cs
+++ PonyChallenge/PonyChallenge/App.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PonyChallenge.Views;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
@@ -11,7 +12,7 @@ namespace PonyChallenge
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NewMazePage();
         }
 
         protected override void OnStart()
