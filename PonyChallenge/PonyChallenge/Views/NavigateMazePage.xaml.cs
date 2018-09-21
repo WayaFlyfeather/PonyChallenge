cf577f35ceb6323d13db2d47b4e6c7ce062d7616
Jon Robinson Levy <jon.r.levy@gmail.com>
2018-09-21 10:39:43 +0200

    Adding and implementing SkiaSharp for drawing maze.

--- PonyChallenge/PonyChallenge/Views/NavigateMazePage.xaml.cs
+++ PonyChallenge/PonyChallenge/Views/NavigateMazePage.xaml.cs
@@ -8,6 +8,9 @@ using System.Threading.Tasks;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+using SkiaSharp;
+using SkiaSharp.Views.Forms;
+
 namespace PonyChallenge.Views
 {
 	[XamlCompilation(XamlCompilationOptions.Compile)]
@@ -21,10 +24,7 @@ namespace PonyChallenge.Views
 			InitializeComponent ();
             prevVM = vm;
             if (vm != null)
-            {
                 vm.PropertyChanged += VM_PropertyChanged;
-                buildMaze();
-            }
 		}
 
         protected override void OnBindingContextChanged()
@@ -35,40 +35,82 @@ namespace PonyChallenge.Views
             if (vm != null)
                 vm.PropertyChanged += VM_PropertyChanged;
             prevVM = vm;
-            buildMaze();
+            MazeCanvas.InvalidateSurface();
         }
 
         private void VM_PropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
         {
             if (String.IsNullOrEmpty(e.PropertyName) || e.PropertyName == nameof(PonyMazeViewModel.LatestSnapshot))
-            {
-                buildMaze();
-            }
+                MazeCanvas.InvalidateSurface();
         }
 
-
-        void buildMaze()
+        void OnMazeCanvasPaintSurface(object sender, SKPaintSurfaceEventArgs args)
         {
-            TheMaze.Children.Clear();
+            SKImageInfo info = args.Info;
+            SKSurface surface = args.Surface;
+            SKCanvas canvas = surface.Canvas;
 
-            if (vm?.LatestSnapshot != null)
+            float WallWidth = 2f;
+
+            canvas.Clear();
+
+            if (vm?.LatestSnapshot == null)
+                return;
+
+            float locationUnit = Math.Min((info.Width - WallWidth * 2) / (float)vm.Width, (info.Height - WallWidth * 2) / (float)vm.Height);
+            float halfLocationUnit = locationUnit / 2f;
+            float quarterLocationUnit = locationUnit / 4f;
+
+            float offsetX = (info.Width - (locationUnit * vm.Width)) / 2f;
+            float offsetY = (info.Height - (locationUnit * vm.Height)) / 2f;
+
+            using (SKPaint wallStroke = new SKPaint
             {
-                for (int y = 0; y < vm.Model.Height; y++)
+                Style = SKPaintStyle.Stroke,
+                Color = Color.Black.ToSKColor(),
+                StrokeWidth = WallWidth,
+                StrokeCap = SKStrokeCap.Square
+            })
+            using (SKPaint ponyFill = new SKPaint
+            {
+                Style = SKPaintStyle.Fill,
+                Color = Color.Red.ToSKColor(),
+            })
+            using (SKPaint exitFill = new SKPaint
+            {
+                Style = SKPaintStyle.Fill,
+                Color = Color.Green.ToSKColor(),
+            })
+            using (SKPaint domokunFill = new SKPaint
+            {
+                Style = SKPaintStyle.Fill,
+                Color = Color.Brown.ToSKColor(),
+            })
+            {
+                canvas.DrawRect(offsetX - WallWidth, offsetY - WallWidth, vm.Width * locationUnit + WallWidth * 2, vm.Height * locationUnit + WallWidth * 2, wallStroke);
+
+                for (int y = 0; y < vm.Height; y++)
                 {
-                    StackLayout row = new StackLayout();
-                    row.HorizontalOptions = LayoutOptions.Fill;
-                    row.VerticalOptions = LayoutOptions.FillAndExpand;
-                    row.Spacing = 0;
-                    row.Orientation = StackOrientation.Horizontal;
-                    for (int x = 0; x < vm.Model.Width; x++)
+                    for (int x = 0; x < vm.Width; x++)
                     {
-                        MazeLocationView locationView = new MazeLocationView()
-                        {
-                            BindingContext = new MazeLocationViewModel(vm.Model.Positions.Locations[x, y]),
-                        };
-                        row.Children.Add(locationView);
+                        if (vm.LatestSnapshot.Locations[x, y].NorthWall)
+                            canvas.DrawLine(new SKPoint() { X = offsetX + (x * locationUnit), Y = offsetY + (y * locationUnit) }, new SKPoint() { X = offsetX + ((x + 1) * locationUnit), Y = offsetY + (y * locationUnit) }, wallStroke);
+                        if (vm.LatestSnapshot.Locations[x, y].EastWall)
+                            canvas.DrawLine(new SKPoint() { X = offsetX + ((x + 1) * locationUnit), Y = offsetY + (y * locationUnit) }, new SKPoint() { X = offsetX + ((x + 1) * locationUnit), Y = offsetY + ((y + 1) * locationUnit) }, wallStroke);
+                        if (vm.LatestSnapshot.Locations[x, y].SouthWall)
+                            canvas.DrawLine(new SKPoint() { X = offsetX + (x * locationUnit), Y = offsetY + ((y + 1) * locationUnit) }, new SKPoint() { X = offsetX + ((x + 1) * locationUnit), Y = offsetY + ((y + 1) * locationUnit) }, wallStroke);
+                        if (vm.LatestSnapshot.Locations[x, y].WestWall)
+                            canvas.DrawLine(new SKPoint() { X = offsetX + (x * locationUnit), Y = offsetY + (y * locationUnit) }, new SKPoint() { X = offsetX + (x * locationUnit), Y = offsetY + ((y + 1) * locationUnit) }, wallStroke);
+
+                        if (vm.LatestSnapshot.Locations[x, y].ContainsPony)
+                            canvas.DrawRect(offsetX + (x * locationUnit) + quarterLocationUnit, offsetY + (y * locationUnit) + quarterLocationUnit, halfLocationUnit, halfLocationUnit, ponyFill);
+
+                        if (vm.LatestSnapshot.Locations[x, y].ContainsDomokun)
+                            canvas.DrawRect(offsetX + (x * locationUnit) + quarterLocationUnit, offsetY + (y * locationUnit) + quarterLocationUnit, halfLocationUnit, halfLocationUnit, domokunFill);
+
+                        if (vm.LatestSnapshot.Locations[x, y].IsExit)
+                            canvas.DrawRect(offsetX + (x * locationUnit) + quarterLocationUnit, offsetY + (y * locationUnit) + quarterLocationUnit, halfLocationUnit, halfLocationUnit, exitFill);
                     }
-                    TheMaze.Children.Add(row);
                 }
             }
         }
