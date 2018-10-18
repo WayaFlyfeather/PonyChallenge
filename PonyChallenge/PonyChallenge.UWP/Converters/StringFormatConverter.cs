a3b352f3679dffb09f222f1abca9c6b12024b354
Jon Robinson Levy <jon.r.levy@gmail.com>
2018-10-18 23:36:30 +0200

    Adding StringFormatConverter to UWP

---
+++ PonyChallenge/PonyChallenge.UWP/Converters/StringFormatConverter.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml.Data;
+
+namespace PonyChallenge.UWP.Converters
+{
+    public class StringFormatConverter : IValueConverter
+    {
+        public string StringFormat { get; set; } = "{0}";
+
+        public StringFormatConverter(string stringFormat)
+        {
+            StringFormat = stringFormat;
+        }
+
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            string format = parameter is null ? StringFormat : parameter.ToString();
+            return String.Format(format, value);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
