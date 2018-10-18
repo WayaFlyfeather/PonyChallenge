19b50fd09e71ce010cdcdbd5652c896bb3d72382
Jon Robinson Levy <jon.r.levy@gmail.com>
2018-10-18 16:32:13 +0200

    Adding ThumbToolTipEffect

---
+++ PonyChallenge/PonyChallenge/Effects/ThumbToolTipEffect.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace PonyChallenge.Effects
+{
+    public class ThumbToolTipEffect : RoutingEffect
+    {
+        public bool Suppressed { get; set; } = true;
+
+        public ThumbToolTipEffect() : base("JonRLevy.ThumbToolTipEffect")
+        { }
+    }
+}
