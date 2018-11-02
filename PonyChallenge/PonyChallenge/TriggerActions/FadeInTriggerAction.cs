416b91d9b9c2f4462abaa652ce82dfaf3919b743
Jon Robinson Levy <jon.r.levy@gmail.com>
2018-11-02 17:48:11 +0100

    Adding FadeIn/FadeOut Triggers to MazeDefinitionFrame

---
+++ PonyChallenge/PonyChallenge/TriggerActions/FadeInTriggerAction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xamarin.Forms;
+
+namespace PonyChallenge.TriggerActions
+{
+    public class FadeInTriggerAction : TriggerAction<VisualElement>
+    {
+        protected async override void Invoke(VisualElement sender)
+        {
+            sender.Opacity = 0.0;
+            sender.IsVisible = true;
+            await sender.FadeTo(1.0);
+        }
+    }
+}
